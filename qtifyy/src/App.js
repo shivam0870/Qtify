73493c72a9e96cdf5ff67d2844562fe59323e85d
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2024-03-26 12:27:35 +0530

    first commit

---
+++ qtifyy/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'; 
+import { FaSearch } from 'react-icons/fa'; 
+import Logo from './assets/logo.png'; 
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="search-box">
+        <input type="text" placeholder="Search an album " />
+        <FaSearch className="search-icon" />
+      </div>
+      <div className="feedback-button">
+        <button>Give Feedback</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
